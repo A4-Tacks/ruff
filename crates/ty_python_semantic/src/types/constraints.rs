9cc0843bcb921bd90ea719d26b07764138396917
Douglas Creager <dcreager@dcreager.net>
2026-04-08 10:17:51 -0400

    [π] 5.1 eliminate infer_map callback plumbing

--- crates/ty_python_semantic/src/types/constraints.rs
+++ crates/ty_python_semantic/src/types/constraints.rs
@@ -2733,7 +2733,7 @@ impl<'db> Type<'db> {
                 if let Some(inferable) = inferable {
                     when = when.remove_noninferable(db, builder, inferable);
                 }
-                PathBounds::compute(db, &builder, when.node)
+                PathBounds::compute(db, builder, when.node)
             })
         }
 
