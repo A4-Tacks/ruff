50b176e2384cf7a312b962ab05997e70a7a56fbc
Brent Westbrook <brentrwestbrook@gmail.com>
2026-04-09 14:42:51 -0400

    actually run and fail a test

--- crates/ruff_linter/tests/mdtest.rs
+++ crates/ruff_linter/tests/mdtest.rs
@@ -13,7 +13,7 @@ fn mdtest(fixture_path: &Utf8Path, content: String) -> datatest_stable::Result<(
     let crate_dir = Utf8Path::new(env!("CARGO_MANIFEST_DIR"));
     let snapshot_path = crate_dir.join("resources").join("mdtest").join("snapshots");
     let absolute_fixture_path = crate_dir.join(fixture_path);
-    let workspace_relative_fixture_path = Utf8Path::new("crates/ty_python_semantic")
+    let workspace_relative_fixture_path = Utf8Path::new("crates/ruff_linter")
         .join(fixture_path.strip_prefix(".").unwrap_or(fixture_path));
 
     let test_name = fixture_path
